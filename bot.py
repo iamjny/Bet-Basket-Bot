8b83c91232ee7a2c53f8b47c4e4d9a46f19c5332
iamjny <jjng435@gmail.com>
2024-02-16 01:18:36 -0500

    Testing sync commands and added comments

--- bot.py
+++ bot.py
@@ -3,8 +3,13 @@ from discord.ext import commands
 import keys
 import logging
 
+extensions = ["cogs.bet_cog",
+              "cogs.track_cog"]
+
 
 def run():
+
+    # Creates instance of a Bot
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix="!", intents=intents)
@@ -12,7 +17,13 @@ def run():
     @bot.event
     async def on_ready():
         print(f'We have logged in as {bot.user}')
-        await bot.load_extension("cogs.bet_cog")
-        await bot.load_extension("cogs.track_cog")
 
+        # Syncing commands to test guild
+        # bot.tree.copy_global_to(guild=discord.Guild)
+
+        # Load multiple cogs
+        for ext in extensions:
+            await bot.load_extension(ext)
+
+    # Replace 'keys.token' with your own token
     bot.run(keys.token, log_handler=handler)
